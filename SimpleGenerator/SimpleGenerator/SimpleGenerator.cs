cd9da2d67daa6f49b2fbcc295bbb1f35b145ea08
Peter Shaw <peter.shaw@digital-solutions.me.uk>
2022-03-28 23:30:26 +0100

    Added simple code gen example

---
+++ SimpleGenerator/SimpleGenerator/SimpleGenerator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SimpleGenerator
+{
+
+  [Generator]
+  public class SimpleGenerator : ISourceGenerator
+  {
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+      // Find the main method
+      var mainMethod = context.Compilation.GetEntryPoint(context.CancellationToken);
+
+      // Build up the source code
+      string source =
+        $@"
+// Auto-generated code
+using System;
+
+namespace {mainMethod.ContainingNamespace.ToDisplayString()}
+{{
+    public partial class {mainMethod.ContainingType.Name}
+    {{
+        public static void GeneratedCall() =>
+            Console.WriteLine($""Generator says: 'Well hello there' I'm in a method in a new partial class."");
+    }}
+}}
+";
+
+      var typeName = mainMethod.ContainingType.Name;
+
+      // Add the source code to the compilation
+      context.AddSource($"{typeName}.generated.cs", source);
+
+    }
+
+  }
+}
---
+++ SimpleGenerator/SimpleGeneratorTest/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MyCompany.Application;
+
+partial class Program
+{
+  static void Main()
+  {
+    Console.WriteLine("Simple Source Generator Test");
+    GeneratedCall();
+  }
+
+  //static partial void GeneratedCall();
+
+}
